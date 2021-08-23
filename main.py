6fd00d0435bd23116c3480beb20fc82981b3977e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-08-23 18:51:08 +0200

    feat: flask flash messages

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ def home():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        if User.query.filter_by(email=request.form.get("email")).first():
+            # User already exists
+            flash("You've already signed up with that email, log-in instead.")
+            return redirect(url_for("login"))
+
         hash_and_salted_password = generate_password_hash(
             request.form.get("password"), method="pbkdf2:sha256", salt_length=8
         )
@@ -68,10 +73,17 @@ def login():
         # Find user by email
         user = User.query.filter_by(email=email).first()
 
-        # Check stored hashed password against entered password hashed
-        if check_password_hash(user.password, password):
+        # Email doesn't exist
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for("login"))
+        # Password incorrect
+        elif not check_password_hash(user.password, password):
+            flash("Password incorrect, please try again.")
+            return redirect(url_for("login"))
+        # Email exists and password correct
+        else:
             login_user(user)
-
             return redirect(url_for("secrets"))
 
     return render_template("login.html")
