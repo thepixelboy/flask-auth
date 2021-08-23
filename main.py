e49903b65c9451891b61138e1b5453ea29f733d1
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-08-23 18:56:39 +0200

    feat: passing authentication status to templates

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class User(UserMixin, db.Model):
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -61,7 +61,7 @@ def register():
 
         return redirect(url_for("secrets"))
 
-    return render_template("register.html")
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -86,13 +86,13 @@ def login():
             login_user(user)
             return redirect(url_for("secrets"))
 
-    return render_template("login.html")
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/secrets")
 @login_required
 def secrets():
-    return render_template("secrets.html", name=current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route("/logout")
